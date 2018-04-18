60b46b6e061b2a73a379462c78566079bbaacb36
[BE] Mai Xuân Tiến <tienmx@vatgia.com>
2018-04-18 14:32:33 +0700

    update

--- src/TraitCrawler/CrawlerTypeTrait.php
+++ src/TraitCrawler/CrawlerTypeTrait.php
@@ -5,22 +5,57 @@
  * Date: 4/15/2018
  * Time: 10:56 PM
  */
+
 namespace Tienmx\Crawler\TraitCrawler;
+
+
 use Tienmx\Crawler\Puppeteer\Puppeteer;
+
 trait CrawlerTypeTrait
 {
+    protected $listUserAgents = array(
+
+        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)    Gecko/20070725 Firefox/2.0.0.6",
+
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
+
+        "Opera/9.20 (Windows NT 6.0; U; en)",
+
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.50",
+
+        "Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.02 [en]",
+
+        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; fr; rv:1.7) Gecko/20040624 Firefox/0.9",
+
+        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/48 (like Gecko) Safari/48"
+
+    );
+
+    protected $listHeader = [
+        'Connection: keep-alive',
+        'Keep-Alive: 300',
+        "User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0.1",
+        "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7",
+        "Accept-Language: vi,vi;q=0.5"
+    ];
+
+
     public function getCrawlerGetContent($data = array())
     {
         $url = isset($data['link']) ? $data['link'] : "";
         $content = file_get_contents($url);
         return $content;
     }
+
     public function getCrawlerCurl($data = array())
     {
         $url = isset($data['link']) ? $data['link'] : "";
-        $content = @$this->__getContent($url);
+        $content = @$this->__getContent($url, $data);
         return isset($content['content']) ? $content['content'] : "";
     }
+
     public function crawlerByPhatom($data = array())
     {
         $return = ['errors' => true, 'msg' => "", 'content' => ''];
@@ -28,6 +63,7 @@ trait CrawlerTypeTrait
             if (!isset($data['link'])) {
                 throw new \Exception('URL or HTML in configuration required', 400);
             }
+
             $browser = new Puppeteer();
             $browser->isDebug = true;
             $result = $browser->html($data);
@@ -41,27 +77,37 @@ trait CrawlerTypeTrait
             $return['msg'] = $exception->getMessage();
         }
         return $return;
+
     }
-    protected function __getContent($url)
+
+    protected function __getContent($url, $data = array())
     {
-        $userAgents = array(
-            "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6)    Gecko/20070725 Firefox/2.0.0.6",
-            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
-            "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)",
-            "Opera/9.20 (Windows NT 6.0; U; en)",
-            "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; en) Opera 8.50",
-            "Mozilla/4.0 (compatible; MSIE 6.0; MSIE 5.5; Windows NT 5.1) Opera 7.02 [en]",
-            "Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; fr; rv:1.7) Gecko/20040624 Firefox/0.9",
-            "Mozilla/5.0 (Macintosh; U; PPC Mac OS X; en) AppleWebKit/48 (like Gecko) Safari/48"
-        );
+        $userAgents = $this->listUserAgents;
+        $head = $this->listHeader;
+
+        $newHeader = isset($data['web_header']) ? $data['web_header'] : "";
+        $newUserAgents = isset($data['web_user_agents']) ? $data['web_user_agents'] : "";
+        /**
+         * @desc Thêm params in trong header
+         */
+        if (!empty($newHeader)) {
+            $newExplodeHead = explode(',', $newHeader);
+            $head = array_merge($head, $newExplodeHead);
+        }
+        $head = array_unique($head);
+        /**
+         * @desc Thêm params trong userAgents
+         */
+        if (!empty($newUserAgents)) {
+            $newExplodeUserAgents = explode(',', $newUserAgents);
+            $userAgents = array_merge($userAgents, $newExplodeUserAgents);
+        }
+        $userAgents = array_unique($userAgents);
+
         $random = rand(0, count($userAgents) - 1);
-        //$user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36\')';
-        $head[] = "Connection: keep-alive";
-        $head[] = "Keep-Alive: 300";
-        $head[] = "User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64; rv:35.0) Gecko/20100101 Firefox/35.0.1";
-        $head[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7";
-        $head[] = "Accept-Language: en-us,en;q=0.5";
+
         $options = array(
+
             CURLOPT_CUSTOMREQUEST => "GET",        //set request type post or get
 //            CURLOPT_POST => false,        //set to GET
             CURLOPT_USERAGENT => $userAgents[$random], //set user agent
