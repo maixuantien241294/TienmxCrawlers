10cdbc0697b73e9ec8e910887ccab4d73f6455d7
Mai Xuân Tiến - IT Vchat <tienmx@vatgia.com>
2018-05-31 12:29:02 +0700

    update

---
+++ src/TraitCrawler/CrawlerLogo.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 5/31/2018
+ * Time: 9:39 AM
+ */
+
+namespace Tienmx\Crawler\TraitCrawler;
+
+
+class CrawlerLogo
+{
+    use BaseTrait;
+
+    public function executeLogo($contentHtml, $linkWebsite, $domain)
+    {
+        $fevicon = '';
+        if (!empty($contentHtml)) {
+            $html = new \DOMDocument();
+            @$html->loadHTML('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' . $contentHtml);
+            $links = $html->getElementsByTagName('link');
+            if ($links->length > 0) {
+                for ($i = 0; $i < $links->length; $i++) {
+                    $link = $links->item($i);
+                    if ($link->getAttribute('rel') == 'icon' || $link->getAttribute('rel') == "Shortcut Icon" || $link->getAttribute('rel') == "shortcut icon") {
+                        $fevicon = $link->getAttribute('href');
+                    }
+                }
+            }
+        }
+        $linkWebsite = $this->getUrl($linkWebsite);
+        $fevicon = $this->__check_url($fevicon,$domain,$linkWebsite);
+        return $fevicon;
+    }
+}
